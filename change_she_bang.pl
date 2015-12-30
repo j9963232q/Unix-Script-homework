7c300211ad49a9458636a3f9fc30155c74b149ee
david <davidtnfsh@gmail.com>
2015-12-30 10:40:45 +0800

    submit homework

---
+++ change_she_bang.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+# convert_she_bang.pl:, change she-bang
+
+$count=1;
+@record;
+while (<>) {
+	# body...	
+	push(@record,$_);
+}
+while()
+open(f,,">")
+foreach $i(@record){
+	print("$i");
+}
--- chkfile.sh
+++ chkfile.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # filetest.sh -- Checks whether files exists and is readable
 [ $# -ne 1 ] && { echo "Usage: $0 file" ; exit 1 ; }
-if [ -f $1 ]; then
-	if [ ! -r $1 ] ; then
+if [ -f $1 ]; then # [ -f $1 ] means whether $1 exist and is a file 
+	if [ ! -r $1 ] ; then # -r means $1 is readable
 		echo "File exists but is not readable"
 	else
 		echo "File is readable"
